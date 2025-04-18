8510e8ec265b0b3122882b4b5ea39c538c517958
steeve02597 <119192572+steeve02597@users.noreply.github.com>
2025-04-18 08:27:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
 SECONDDB_URI = environ.get('SECONDDB_URI', None)
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://steevebgeorge:2FcVPvpQNbmE4fbo@cluster0.mc7qre3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
